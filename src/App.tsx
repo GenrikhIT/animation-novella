4eec2e4ded30689109c65140088d9a9829b75a8a
Genrikh <lessergv@gmail.com>
2024-08-12 18:25:51 +0400

    All

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import sowa from './sowa.png';
+import minion from './minion.png'
+import bubble from './bubble.png'
+import Typewriter from 'typewriter-effect';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="First">
+          <div className='First_box'>
+          <Typewriter
+  options={{
+    strings: ['Наведи на меня'],
+    autoStart: true,
+    loop: true,
+  }}
+/>
+            
+            <img src={minion} width={160}/>
+            <div className='bubble'>Привет!</div>
+          </div>
+      </div>
+      <div className="Second">
+        <div className='Second_box'>Наведи на меня
+        <img src={sowa} width={160}/>
+        <img src={bubble} alt="Your Image" className="zoom-image"></img>
+        </div>
+      </div>
+
     </div>
   );
 }
